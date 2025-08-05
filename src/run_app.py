a5f9f15252f9492dc487373ce7e3429b8c6c3dd0
Artyom Kadikov <8961805@gmail.com>
2025-08-05 20:09:14 +0300

    Добавлен контекстный менеджер жизненного цикла приложения FastAPI

--- src/run_app.py
+++ src/run_app.py
@@ -1,11 +1,35 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Any
+
 from fastapi import FastAPI
 import uvicorn
 
 from api.routes import router
 from core.config import settings
+from src.core.models.db_helper import db_helper
+from src.core.models import Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
+    """Контекстный менеджер жизненного цикла приложения FastAPI
+
+    Выполняет:
+    1. Инициализацию базы данных
+    2. Корректное освобождение ресурсов БД при завершении
+    """
+
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await db_helper.engine.dispose()
+
 
 app = FastAPI(
     default_response_class=settings.api.default_response_class,
+    lifespan=lifespan,
     title=settings.api.docs_title,
     description=settings.api.docs_description,
 )
