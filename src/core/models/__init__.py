818212ed8675188279c94b906d1690ff46b10077
Artyom Kadikov <8961805@gmail.com>
2025-08-05 20:06:33 +0300

    init

---
+++ src/core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Tenders",
+)
+
+from .model_base import Base
+from .db_helper import DatabaseHelper, db_helper
+from .model_tenders import Tenders
